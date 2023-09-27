cf41608709d464ca1583fdc84508d2a10c1133a0
Deucaliongit <ilhamrifai166@gmail.com>
2023-09-27 09:24:46 +0700

    perubahan halaman depan

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -16,20 +16,20 @@ const Slider = () => {
         initial={{ opacity: 0, duration: 10 }}
         animate={{ opacity: 1, duration: 10 }}
         exit={{ opacity: 0 }}
-        transition={{ ease: "easeIn", duration: 4 }}
+        transition={{ ease: "easeIn", duration: 2 }}
         className="aspect-w-16 aspet-h-9 relative"
       >
         <img src={images[3]} alt="slider" />
-        <div className="absolute py-2.5 md:left-10 lg:left-20 left-5 top-5 lg:top-1/4  capitalize text-sky-500 text-xs sm:text-sm md:text-xl font-semibold text-center leading-4">
+        <div className="hidden md:inline-block absolute py-2.5 md:left-10 lg:left-20 left-5 top-5 lg:top-1/4  capitalize text-sky-500 text-xs sm:text-sm md:text-xl font-semibold text-center leading-4">
           Kyoai medical services
         </div>
-        <div className="max-w-xs lg:max-w-lg absolute block md:left-10 lg:left-20 left-5 top-[32%] capitalize text-sky-500 text-xs sm:text-lg md:text-4xl lg:font-black lg:text-6xl font-bold text-left leading-4">
+        <div className="hidden max-w-xs lg:max-w-lg absolute md:inline-block md:left-10 lg:left-20 left-5 top-[32%] capitalize text-sky-500 text-xs sm:text-lg md:text-4xl lg:font-black lg:text-6xl font-bold text-left leading-4">
           <h1>YOUR HEALTH</h1>
           <p>IS OUR PRIORITY</p>
-          <p className="max-w-[140px] lg:max-w-[400px] text-xs lg:text-xl font-normal lg:mt-2">
+          {/* <p className="max-w-[140px] lg:max-w-[400px] text-xs lg:text-xl font-normal lg:mt-2">
             For Japanese people living in Indonesia, We provide safe and secure
             medical care.
-          </p>
+          </p> */}
         </div>
       </motion.div>
     </div>
